e66e11ea8fd40b46f946492a419f08dfef330c96
Grzegorz Przybyła <grzegorz.przybyla@icloud.com>
2017-05-24 08:06:03 +0200

    Add missing calculate path metric method

--- core/net/rpl/rpl-gp-of.c
+++ core/net/rpl/rpl-gp-of.c
@@ -37,6 +37,24 @@ static void reset(rpl_dag_t *sag) {
     PRINTF("RPL: GP - Objective Function Reset");
 }
 
+static rpl_path_metric_t
+calculate_path_metric(rpl_parent_t *p)
+{
+    if(p == NULL) {
+        return MAX_PATH_COST * RPL_DAG_MC_ETX_DIVISOR;
+    }
+
+#if RPL_DAG_MC == RPL_DAG_MC_NONE
+    return p->rank + (uint16_t)p->link_metric;
+#elif RPL_DAG_MC == RPL_DAG_MC_ETX
+    return p->mc.obj.etx + (uint16_t)p->link_metric;
+#elif RPL_DAG_MC == RPL_DAG_MC_ENERGY
+  return p->mc.obj.energy.energy_est + (uint16_t)p->link_metric;
+#else
+#error "Unsupported RPL_DAG_MC configured. See rpl.h."
+#endif /* RPL_DAG_MC */
+}
+
 static rpl_parent_t *best_parent(rpl_parent_t *p1, rpl_parent_t *p2)
 {
     rpl_dag_t *dag;
